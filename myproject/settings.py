438d184cd2db098f5089ec00ae97651aa0d60926
Anika Hossain <168166772+Anika2121@users.noreply.github.com>
2025-04-08 01:08:01 +0600

    Add missing dj_database_url import in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-
+import dj_database_url
+from celery.schedules import crontab
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
