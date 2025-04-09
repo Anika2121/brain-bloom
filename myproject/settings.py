19fd7cf5d3da23d5db8fa0c4b4db348c3d8ef523
Anika Hossain <168166772+Anika2121@users.noreply.github.com>
2025-04-09 15:35:16 +0600

    Update settings.py for Render environment variables

--- myproject/settings.py
+++ myproject/settings.py
@@ -198,8 +198,10 @@ LOGGING = {
 
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Redis as the message broker
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Redis as the message broker
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('REDIS_URL')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
